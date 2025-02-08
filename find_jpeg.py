fe4e84bb820294b20855bc6e9d1ac9e60b6a6d0a
mtenslin <mait.tenslind@ut.ee>
2025-02-08 16:45:19 +0200

    script downloads and extracts the files

--- find_jpeg.py
+++ find_jpeg.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 
 import os
+import requests
+from zipfile import ZipFile
+# Download the zipfile
+download_url = "https://upload.itcollege.ee/~aleksei/random_files_without_extension.zip"
 
+request = requests.get(download_url)
+with open("random_files.zip", "wb") as zipfile:
+    zipfile.write(request.content)
+
+# extract the zipfile
+with ZipFile("random_files.zip", 'r') as zip:
+    zip.extractall()
+# Rename jpegs with correct extensions
 jpeg_signature = bytes([0xFF, 0xD8])
 directory = r'random_files'
 
